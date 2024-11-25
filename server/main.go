3537def8a66bfaf8be5f42c4a15a217e972655c9
xiwangxue <xiwangxue@xiwangxuedeMacBook-Pro.local>
2024-11-25 11:13:45 +0800

    server加上存储路径初始化

--- server/main.go
+++ server/main.go
@@ -2,11 +2,32 @@ package main
 
 import (
 	"cc/server/app"
+	"cc/server/configs"
+	"fmt"
+	"os"
 )
 
 func main() {
-	err := app.InitRouter().Run(":9000")
+	r := app.InitRouter()
+	err := InitDir(configs.DataPrefix)
 	if err != nil {
 		panic(err)
 	}
+	err = r.Run(":9000")
+	if err != nil {
+		panic(err)
+	}
+}
+
+func InitDir(dirName string) error {
+	if _, err := os.Stat(dirName); os.IsNotExist(err) {
+		// 目录不存在，创建目录
+		err := os.Mkdir(dirName, 0755) // 0755是目录权限设置
+		if err != nil {
+			// 创建目录失败
+			fmt.Printf("Failed to create directory: %s\n", err)
+			return err
+		}
+	}
+	return nil
 }
